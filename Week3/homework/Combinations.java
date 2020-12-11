063eda322cefbc516cca82d3b9d04aff76257916
ssj <sjsuijax@163.com>
2020-12-11 14:19:51 +0800

    week03 commit

--- Week3/homework/Combinations.java
+++ Week3/homework/Combinations.java
@@ -6,20 +6,24 @@ import java.util.List;
  * @create 2020-12-06 22:43
  */
 public class Combinations {
-    private List<List<Integer>> ans = new ArrayList<>();
+    private List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> combine(int n, int k) {
-        getCombine(n, k, 1, new ArrayList<>());
-        return ans;
+        backtracking(n, k, 1, new ArrayList<>());
+        return res;
     }
-    private void getCombine(int n, int k, int start, List<Integer> list) {
-        if(k == 0) {
-            ans.add(new ArrayList<>(list));
+    //1)确定回溯的参数
+    private void backtracking(int n, int k, int start, List<Integer> path) {
+        //2)终止条件（同时收集结果）
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
             return;
         }
-        for(int i = start;i <= n - k + 1;i++) {
-            list.add(i);
-            getCombine(n, k - 1, i+1, list);
-            list.remove(list.size() - 1);
+        //3)单层搜索的过程
+        //4)剪枝
+        for (int i = start; i <= n - (k - path.size()) + 1; i++) {
+            path.add(i);
+            backtracking(n, k, i + 1, path);
+            path.remove(path.size() - 1);
         }
     }
 }
