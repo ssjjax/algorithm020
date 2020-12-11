9e98d5b810ec57bd6736c6cad7c7ba3cb245060b
ssj <sjsuijax@163.com>
2020-12-11 15:21:07 +0800

    week03 commit

--- Week3/homework/Permutations.java
+++ Week3/homework/Permutations.java
@@ -7,24 +7,30 @@ import java.util.List;
  * @create 2020-12-06 22:46
  */
 public class Permutations {
+    private List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        List<Integer> output = new ArrayList<Integer>();
-        for (int num : nums) {
-            output.add(num);
-        }
-        int n = nums.length;
-        backtrack(n, output, res, 0);
+        backtracking(nums, new boolean[nums.length], new ArrayList<>());
         return res;
     }
-    private void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
-        if (first == n) {
-            res.add(new ArrayList<Integer>(output));
+    //1)确定回溯的参数
+    private void backtracking(int[] nums, boolean[] used, List<Integer> path) {
+        //2)终止条件（同时收集结果）
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
         }
-        for (int i = first; i < n; i++) {
-            Collections.swap(output, first, i);
-            backtrack(n, output, res, first + 1);
-            Collections.swap(output, first, i);
+        //3)单层搜索的过程
+        //（注：与组合问题的区别：1 组合在for循环时需要start条件，而排列需要一个boolean used[]
+        //                        2 排列不需要剪枝）
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] == true)  {
+                continue;
+            }
+            used[i] = true;
+            path.add(nums[i]);
+            backtracking(nums, used, path);
+            path.remove(path.size() - 1);
+            used[i] = false;
         }
     }
 }
