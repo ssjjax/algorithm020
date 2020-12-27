ac44e1b013c2f5fe11d7dd644bfbe0b09f80531a
suisijia <sjsuijax@163.com>
2020-12-28 00:11:14 +0800

    week06 commit

---
+++ Week6/homework/PalindromicSubstrings.java
@@ -0,0 +1,19 @@
+/**
+ * @author Suisijia
+ * @create 2020-12-27 23:40
+ */
+public class PalindromicSubstrings {
+    public int countSubstrings(String s) {
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int ans = 0;
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = 0; i <= j; i++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
