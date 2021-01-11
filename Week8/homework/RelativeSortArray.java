7ba140d23cdbb7b80a26fb3eecd0a2a5aa83b4a0
ssj <sjsuijax@163.com>
2021-01-11 10:22:42 +0800

    week08 commit

---
+++ Week8/homework/RelativeSortArray.java
@@ -0,0 +1,27 @@
+/**
+ * @author Suisijia
+ * @create 2021-01-11 10:20
+ */
+//计数排序
+public class RelativeSortArray {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] count = new int[1001];
+        for (int num1 : arr1) {
+            count[num1]++;
+        }
+        int i = 0;
+        for (int num2 : arr2) {
+            while (count[num2] > 0) {
+                arr1[i++] = num2;
+                count[num2]--;
+            }
+        }
+        for (int num1 = 0; num1 < count.length; num1++) {
+            while (count[num1] > 0) {
+                arr1[i++] = num1;
+                count[num1]--;
+            }
+        }
+        return arr1;
+    }
+}
